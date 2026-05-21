66e2610786d02cc48ea637014a5dff82617dff0f
ellensallberg <Ellen.sallberg1@gmail.com>
2026-05-21 14:15:46 +0200

    Assignment 7 JS

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,10 @@ const settingsForm = document.getElementById("settings-form");
 const settingsBtn = document.getElementById("settings-btn");
 const difficultySelect = document.getElementById("difficulty");
 
+const restartBtn = document.getElementById("restart-btn");
+const finalScore = document.getElementById("final-score");
+
+
 // Array
 const words = [
   "dependent",
@@ -32,11 +36,61 @@ const words = [
   "north",
 ];
 
-//Initializing word
-let randomWord;
+randomizeWord()
 
-//Initializing score
+//Randomize word
+function randomizeWord() {
+  let randomWord = function(array) {
+  return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function addWordToDom() {
+    word.textContent = (randomWord(words))
+  }
+
+  addWordToDom()
+}
+
+//Scoring
 let score = 0;
+function updateScore() {
+  score++
+  scoreEl.textContent = (score)
+}
 
-//Initializing time
+//Timer
 let time = 10;
+function countdown() {
+    (time--)
+      if (time < 0) {
+        finalScore.textContent = (scoreEl.textContent)
+        endgameEl.style.display = "inline";
+
+        clearInterval(timer)
+      } 
+      if (time >= 0) {
+        displayTime()
+      }
+}
+
+const timer = setInterval(countdown, 1000)
+
+//display time
+function displayTime() {
+  timeEl.textContent = (time + "s")
+}
+
+//add 5s to timer
+function addTime() {
+  time += 5
+}
+ 
+//input event listener
+text.addEventListener("input", function() {
+  if (text.value === word.textContent) {
+    text.value = ""
+    randomizeWord()
+    updateScore()
+    addTime()
+  }
+})
